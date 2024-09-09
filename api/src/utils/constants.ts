3461b6c2d03fe185f5a03f5d9cc29563ef80cd26
gtarragona <gtarragona@switchfly.com>
2024-09-08 22:36:06 -0700

    Declared new constants in constants file.

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const JWT_KEY = process.env.JWT_KEY || 'secret';
 export const TASK_STATUS = ['pending', 'in progress', 'completed'];
+export const PORT = process.env.PORT || 3000;
+export const MONGO_URI = process.env.MONGO_URI || '';
+export const RATE_LIMITER_WINDOW_MS = 10 * 60 * 1000; // 10 minutes

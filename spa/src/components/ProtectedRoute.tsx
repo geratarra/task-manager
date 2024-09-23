b8324a63f2f3d7e42fe570fe87f574b13293cb1f
gtarragona <gtarragona@switchfly.com>
2024-09-22 22:09:37 -0700

    Modified ProtectedRoute logic to now call /verify-endpoint to retrieve and store token. This is to avoid losing the token when refreshing the page.

--- spa/src/components/ProtectedRoute.tsx
+++ spa/src/components/ProtectedRoute.tsx
@@ -1,10 +1,32 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../utils/AuthProvider";
+import axios from "axios";
+import { API_URI } from "../utils/constants";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }): React.ReactElement {
-    const { token } = useContext(AuthContext);
+    const { token, login } = useContext(AuthContext);
     const isAuthenticated = !!token;
+    const navigate = useNavigate();
+    let { pathname } = useLocation();    
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            if (!isAuthenticated) {
+                try {
+                    const response = await axios.get(API_URI + '/auth/verify-token', { withCredentials: true });
+                    if (response.status === 200) {
+                        login(response.data.token);
+                        navigate(pathname);
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        };
+
+        checkAuth();
+    }, [token]);
 
     return (
         <>

5574be16bbbdd3bc4915942e842c2b216770b7ab
gtarragona <gtarragona@switchfly.com>
2024-10-01 14:09:24 -0700

    Added logout error banner in Home component.

--- spa/src/routes/Home.tsx
+++ spa/src/routes/Home.tsx
@@ -1,34 +1,37 @@
 import axios from 'axios';
 import TaskList from '../components/TaskList';
 import { API_URI } from '../utils/constants';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../utils/AuthProvider';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 function Home() {
   const { logout } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [error, setError] = useState<string>();
 
   const handleLogout = async () => {
     try {
-      const response = await axios.post(`${API_URI}/auth/logout`, {
+      const response = await axios.post(`${API_URI}/auth/logout`, {}, {
         withCredentials: true
       });
 
-      if (response.status === 200) {
+      if (response.status === 200 || response.status === 204) {
         logout();
         navigate('/login');
       } else {
         console.error('Logout failed:', response);
+        setError('Failed to logout. Please try again.');
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error('Logout error:', error);
+      setError(error.response?.data?.message || 'Failed to logout. Please try again.');
     }
   };
 
   return (
     <div>
-      <nav className="level">
+      <nav className="level p-5">
         <div className="level-left">
           <div className="level-item">
             <h2 className='title is-2'>Task Manager</h2>
@@ -43,6 +46,9 @@ function Home() {
           </div>
         </div>
       </nav>
+      <div className='block container'>
+          {error && <div className='block notification is-danger'>{error}</div>}
+      </div>
       <Outlet />
     </div>
   );

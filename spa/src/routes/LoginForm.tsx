980dd67e9b0ad1fe2a69fd03088b7917ccf4dd50
gtarragona <gtarragona@switchfly.com>
2024-10-01 14:10:40 -0700

    Added form validation to LoginForm.

--- spa/src/routes/LoginForm.tsx
+++ spa/src/routes/LoginForm.tsx
@@ -2,7 +2,10 @@ import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../utils/AuthProvider';
 import axios from 'axios';
-import { API_URI } from '../utils/constants';
+import { API_URI, PASSWORD_REGEX } from '../utils/constants';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { AuthInputs } from '../utils/types';
+
 
 function LoginForm() {
     const [email, setEmail] = useState<string>('');
@@ -10,9 +13,13 @@ function LoginForm() {
     const [error, setError] = useState<string | null>(null);
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AuthInputs>({ mode: 'onBlur' });
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const loginSubmit: SubmitHandler<AuthInputs> = async (formData) => {
         setError(null);
 
         try {
@@ -20,6 +27,8 @@ function LoginForm() {
             const response = await axios.post(loginUrl, {
                 email,
                 password,
+            }, {
+                withCredentials: true
             });
 
             const token = response.data.token;
@@ -37,35 +46,51 @@ function LoginForm() {
     };
 
     return (
-        <div className='container'>
+        <div className='container py-5 is-flex is-flex-direction-column is-align-items-center'>
             <h2 className='title is-2'>Login</h2>
-            <form onSubmit={handleSubmit} className='block'>
+            <form onSubmit={handleSubmit(loginSubmit)} className='block column is-half'>
                 <div className='field'>
-                    <label htmlFor="email">Email:</label>
+                    <label className='label' htmlFor="email">Email:</label>
                     <div className='control'>
                         <input
+                            className="input"
                             type="email"
                             id="email"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
+                            {...register("userEmail", { required: true })}
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
                     </div>
+                    {errors.userEmail && <span className='help is-danger'>This field is required</span>}
                 </div>
                 <div className='field'>
-                    <label htmlFor="password">Password:</label>
+                    <label className='label' htmlFor="password">Password:</label>
                     <div className='control'>
                         <input
+                            {...register("password", {
+                                required: true,
+                                minLength: 8,
+                                pattern: PASSWORD_REGEX
+                            })}
+                            className="input"
                             type="password"
                             id="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)} />
                     </div>
+                    {errors.password?.type === 'required' && <span className='help is-danger'>This field is required</span>}
+                    {errors.password?.type === 'minLength' && <span className='help is-danger'>Password must be at least 8 characters long</span>}
+                    {errors.password?.type === 'pattern' && <span className='help is-danger'>Password must contain at least one uppercase letter, one number, and one special character</span>}
                 </div>
                 <div className='field'>
-                    <div className='control'>
+                    <div className='control is-flex is-flex-direction-column is-align-items-center'>
                         <button className='button is-primary' type="submit">Login</button>
                     </div>
                 </div>
             </form>
+            <div className='block'>
+                <p>Don't have an account? <a href="/signup">Signup</a></p>
+            </div>
             <div className='block'>
                 {error && <div className='block notification is-danger'>{error}</div>}
             </div>
--- spa/src/utils/constants.ts
+++ spa/src/utils/constants.ts
@@ -1,1 +1,2 @@
 export const API_URI = process.env.REACT_APP_API_URI;
+export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\.])[A-Za-z\d@$!%*?&\.]{8,}$/;
